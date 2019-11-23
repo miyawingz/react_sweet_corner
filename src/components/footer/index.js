80b03eab6f58b7ac57681d1b78614e167e5f0c81
Mia Zhang <miyawingz@gmail.com>
2019-11-22 23:47:06 -0800

    completed feature set 3 - footer section

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import dots from '../../assets/images/dots-footer.png';
+import phone from '../../assets/images/phone.png';
 
 import './footer.scss';
 
-export default props =>{
+export default props => {
+    const year = (new Date()).getFullYear();
+
     return (
-        <div>
-            <h1 className="center">This is a footer</h1>
+        <div className="footer">
+            <div className="dots">
+                <img src={dots} />
+            </div>
+            <div className="center">Copyright &copy; {year} Sweet Corner. All rights reserved</div>
+            <div className="contact">
+                <img src={phone} />
+                <p>800 264 2699</p>
+            </div>
         </div>
     )
 }
