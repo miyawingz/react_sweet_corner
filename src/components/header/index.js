99f43695fb7ad2a3c038734a375ba2fc47b56070
Mia Zhang <miyawingz@gmail.com>
2019-11-22 22:40:30 -0800

    completed feature set 2 - header section

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Nav from '../nav';
 
 import './header.scss';
+import headerImage from '../../assets/images/header.png';
+import logoImage from '../../assets/images/logo.png';
 
-export default props =>{
+export default props => {
     return (
-        <div>
-            <h1 className="center">This is a header</h1>
+        <div className="header">
+            <img src={headerImage} />
+            <Nav />
+            <img src={logoImage} />
+            <p className="center">We delivery cupcakes for the important events of your life!</p>
         </div>
     )
 }
