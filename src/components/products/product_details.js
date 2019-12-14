92cb8a88f57b7196834cea712f579fea85642bba
Mia Zhang <miyawingz@gmail.com>
2019-12-14 09:54:19 -0800

    complete v3 feature 1, set up quantity btn

--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -5,14 +5,44 @@ import Money from '../general/money';
 import './product_details.scss';
 
 class ProductDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quantity: 1
+        }
+    }
+
     componentDidMount() {
         const { getProductDetails, match: { params } } = this.props;
         getProductDetails(params.product_id);
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         this.props.clearProductDetails();
     }
+
+
+    decrementQuantity() {
+        const { quantity } = this.state;
+        if (quantity > 1) {
+            this.setState({
+                quantity: quantity - 1
+            })
+        }
+    }
+
+    handleAddToCart() {
+        console.log(`add ${this.state.quantity} itme with id ${this.props.details.id}`)
+    }
+
+
+    incrementQuantity() {
+        const { quantity } = this.state;
+        this.setState({
+            quantity: quantity + 1
+        })
+    }
+
 
     render() {
         const { details } = this.props;
@@ -26,10 +56,18 @@ class ProductDetails extends React.Component {
                     <div className="content">
                         <p className="name">{name}</p>
                         <p className="caption">{caption}</p>
-                        <p className="description-title">Description</p>
+                        <p className="subtitle">Description</p>
                         <p className="description">{description}</p>
                         <Money className="cost" penny={cost} />
+                        <p className="subtitle">Quantity</p>
+                        <div className="cart-quantity">
+                            <button className="btn-quantity" onClick={this.decrementQuantity.bind(this)}>-</button>
+                            <div className="quantity">{this.state.quantity}</div>
+                            <button className="btn-quantity" onClick={this.incrementQuantity.bind(this)}>+</button>
+                            <button className="addToCart" onClick={this.handleAddToCart.bind(this)}>Add To Cart</button>
+                        </div>
                     </div>
+
                 </div>
             )
         }
